9455a12c8aaa6c91bce3451d041fc112299a4fe1
ligade <sourabhligade07@gmail.com>
2024-03-29 23:20:07 +0530

    Initial commit

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    title=models.CharField(max_length=343)
+    description=models.CharField
+    image=models.ImageField(upload_to='projectimages/')
+
+    def __str__(self) -> str:
+        return self.title   
