53d99c784bfc97b4e3ea7025c90e85e07de264fa
ligade <sourabhligade07@gmail.com>
2024-03-30 23:14:33 +0530

    updates

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('', include('portfolio.urls')),  # Redirect to portfolio app
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
 
