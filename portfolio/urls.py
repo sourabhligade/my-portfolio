69bde17995b97fe30f517100e6291f6fe610568b
ligade <sourabhligade07@gmail.com>
2024-04-05 23:28:30 +0530

    ok

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns=[
 
     path('',views.portfolio,name='portfolio'),
+    path('project/<int:pk>/', views.project_detail, name='project_detail'),
+
+
 
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,3 +6,6 @@ def portfolio(request):
     projects=Project.objects.all()
     return render(request,'portfolio/portfolio.html',{'projects':projects})
 
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'project_detail.html', {'project': project})
