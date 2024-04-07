934644ca8072d27c9db855c2c78065e88c1a83d2
ligade <sourabhligade07@gmail.com>
2024-04-07 22:26:10 +0530

    7

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from .models import Project
 # Create your views here.
 
+
+
 def portfolio(request):
     projects=Project.objects.all()
     return render(request,'portfolio/portfolio.html',{'projects':projects})
