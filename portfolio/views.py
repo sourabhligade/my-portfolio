6c5cd121df84dc6b93ff3530ee03d86898cdd9ae
ligade <sourabhligade07@gmail.com>
2024-04-09 17:52:52 +0530

    added logo

--- portfolio/views.py
+++ portfolio/views.py
@@ -10,4 +10,5 @@ def portfolio(request):
 
 def project_detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-    return render(request, 'project_detail.html', {'project': project})
+    template_name = 'portfolio/wormyreads_detail.html'  # Template for Wormy Reads project
+    return render(request, 'project_detail.html', {'project': project, 'template_name': template_name})
