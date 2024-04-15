cb480a72052a1c396a3d72a6a93fcfd3e5534644
ligade <sourabhligade07@gmail.com>
2024-04-15 23:35:52 +0530

    added dialect

--- portfolio/views.py
+++ portfolio/views.py
@@ -11,4 +11,6 @@ def portfolio(request):
 def project_detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
     template_name = 'portfolio/wormyreads_detail.html'  # Template for Wormy Reads project
+    template_name = 'portfolio/dialect.html'  # Template for Wormy Reads project
+
     return render(request, 'project_detail.html', {'project': project, 'template_name': template_name})
