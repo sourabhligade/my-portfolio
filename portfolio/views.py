60d09b326fd593fef5ca0c6c460d36bebc7dce71
ligade <sourabhligade07@gmail.com>
2024-04-22 11:00:59 +0530

    added index

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,7 +6,7 @@ from .models import Project
 
 def portfolio(request):
     projects=Project.objects.all()
-    return render(request,'portfolio/portfolio.html',{'projects':projects})
+    return render(request,'portfolio/index.html',{'projects':projects})
 
 def project_detail(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
