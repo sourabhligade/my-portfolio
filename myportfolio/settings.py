77468b191e26f63979ea3cedd310eb942a0880f7
ligade <sourabhligade07@gmail.com>
2024-04-24 14:12:30 +0530

    added vercel

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -25,7 +25,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 # SECURITY WARNING: Set ALLOWED_HOSTS to your domain(s)
-ALLOWED_HOSTS = ['example.com', 'www.example.com', '127.0.0.1']
+ALLOWED_HOSTS = ['vercel.app', 'www.example.com', '127.0.0.1']
 
 # Application definition
 
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation
