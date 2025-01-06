47e8c1136e6656eb68b108dd2117aca33baf2316
ligade <sourabhligade07@gmail.com>
2025-01-06 23:59:00 +0530

    Your commit message

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -6,7 +6,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'juv^rw+x=t(#v4q0#*-jd$+y!4&--ct544-76=+=zx2ls0#*md'
 
 DEBUG = True
